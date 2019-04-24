4e5cc562a26ff062aa19a031c577c3a67f173804
adamzimmerman <arz2@illinois.edu>
2019-04-23 23:10:05 -0500

    e

--- app/src/main/java/com/example/snake/MainActivity.java
+++ app/src/main/java/com/example/snake/MainActivity.java
@@ -4,20 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     *
+     */
+.setOnClickListener() {
+        opentogame();
+        opentoleaderboard();
+    }
+
+    public void open() {
+        Intent what = new Intent(this, LeaderBoard);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (end) {
+            setContentView(R.layout.activity_leaderboard);
+        }
     }
-}
 
-
-.setOnClickListener() {
-    opentogame();
-    opentoleaderboard();
-}
-
-public void open() {
-    Intent what = new Intent(this, LeaderBoard);
 }
